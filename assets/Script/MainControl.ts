230c25c76f138680fddfa3c53032898ea04356e9
L2Nam <99789427+L2Nam@users.noreply.github.com>
2022-08-01 09:15:08 +0700

    New Update

--- assets/Script/MainControl.ts
+++ assets/Script/MainControl.ts
@@ -15,6 +15,7 @@ export default class MainControl extends cc.Component {
     @property(cc.Prefab)
     PlatformPrefab: cc.Prefab = null;
     Platform: cc.Node[] = [null, null]
+    Platform_parent: cc.Node
     min_plat = -115
     max_plat = 80
     min_scale_plat = 0.4
@@ -30,22 +31,36 @@ export default class MainControl extends cc.Component {
 
     game_over: cc.Sprite
     gameStatus: GameStatus = GameStatus.Game_Ready
+    score: cc.Label
+    score_num = 0
+    Best_score: cc.Label
+    Best_score_num = 0
+    Best_score_img: cc.Sprite
+    replay: cc.Button
+
+    // anim_monkey_walk: cc.Animation
 
     onLoad() {
         cc.Canvas.instance.node.on(cc.Node.EventType.TOUCH_START, this.onTouchStart, this);
         cc.Canvas.instance.node.on(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
+        this.Platform_parent = this.node.getChildByName("Platform")
         this.cloud0 = this.node.getChildByName("Cloud0").getComponent(cc.Sprite)
         this.cloud1 = this.node.getChildByName("Cloud1").getComponent(cc.Sprite)
         this.stick = this.node.getChildByName("Stick").getComponent(cc.Sprite)
         this.monkey = this.node.getChildByName("Monkey").getComponent(cc.Sprite)
         this.game_over = this.node.getChildByName("GameOver").getComponent(cc.Sprite)
+        this.score = this.node.getChildByName("Score_label").getComponent(cc.Label)
+        this.Best_score = this.node.getChildByName("Best_Score_label").getComponent(cc.Label)
+        this.Best_score_img = this.node.getChildByName("Best_Score").getComponent(cc.Sprite)
+        this.replay = this.node.getChildByName("Replay").getComponent(cc.Button)
+        this.replay.node.on(cc.Node.EventType.TOUCH_END, this.replay_btn, this)
     }
 
     start() {
-        // start Platform
+        // make platform
         for (let i = 0; i < this.Platform.length; i++) {
             this.Platform[i] = cc.instantiate(this.PlatformPrefab);
-            this.node.addChild(this.Platform[i]);
+            this.node.getChildByName("Platform").addChild(this.Platform[i]);
         }
         this.Platform[0].x = -115;
         this.Platform[1].scaleX = Math.random() * (this.max_scale_plat - this.min_scale_plat) + this.min_scale_plat
@@ -56,6 +71,11 @@ export default class MainControl extends cc.Component {
     update(dt) {
         if (this.gameStatus !== GameStatus.Game_Playing) {
             return;
+        }
+
+        if (this.score_num > this.Best_score_num) {
+            this.Best_score_num = this.score_num
+            this.Best_score.string = this.Best_score_num.toString()
         }
 
         // Cloud
@@ -70,8 +90,9 @@ export default class MainControl extends cc.Component {
             this.cloud1.node.x = 250
         }
 
+        // click chuột làm dài gậy, nhả chuột gậy rơi xuống ngang
         if (this.is_longer) {
-            this.stick.node.scaleY += 0.2
+            this.stick.node.scaleY += 0.4
         }
         else
             this.stick_length = this.stick.node.height * this.stick.node.scaleY
@@ -79,9 +100,11 @@ export default class MainControl extends cc.Component {
             this.stick.node.angle -= 3
         }
 
+
+        // dưng rơi, check die
         if (this.stick.node.angle <= -86) {
             if (this.monkey.node.x <= -100 + this.stick_length)
-                this.monkey.node.x += 2
+                this.monkey.node.x += 3
             if (this.monkey.node.x >= -100 + this.stick_length) {
                 if (this.check_die()) {
                     this.monkey_die = true
@@ -89,9 +112,14 @@ export default class MainControl extends cc.Component {
                     // cc.tween(this.monkey.node).by(2, { y: -400 }).call(() => {
                     //     this.die()
                     // }).start()
-                    cc.tween(this.monkey.node).by(2, { y: -400 }).start()
-                    cc.tween(this.monkey.node).by(2, { angle: -270 }).start()
+                    // cc.tween(this.stick.node).by(2, { angle: -110 }).start()
+                    cc.tween(this.monkey.node).by(2, { y: -400, angle: -270 }).start()
+                    this.die()
 
+                }
+                if (!this.monkey_die) {
+                    this.score_num++
+                    this.score.string = this.score_num.toString()
                 }
                 this.stick.node.active = false
                 this.stick.node.scaleY = 1
@@ -101,6 +129,7 @@ export default class MainControl extends cc.Component {
             }
         }
 
+        // new platform
         if (!this.monkey_die) {
             if (this.Platform[this.plat_number].x >= -115) {
                 for (let i = 0; i < this.Platform.length; i++) {
@@ -118,8 +147,10 @@ export default class MainControl extends cc.Component {
     }
 
     onTouchStart(event: cc.Event.EventTouch) {
-        this.is_longer = true
-        this.stick.node.active = true
+        if (!this.monkey_die) {
+            this.is_longer = true
+            this.stick.node.active = true
+        }
     }
     onTouchEnd(event: cc.Event.EventTouch) {
         this.is_longer = false
@@ -137,6 +168,34 @@ export default class MainControl extends cc.Component {
         this.scheduleOnce(() => {
             this.game_over.node.active = true
             this.gameStatus = GameStatus.Game_Over
+            this.monkey_die = true
+            this.Best_score_img.node.active = true
+            this.Best_score.node.active = true
+            this.replay.node.active = true
+            this.Platform_parent.active = false
+            // this.stick.node.active = false
         }, 2)
+    }
+
+    replay_btn() {
+        this.Platform[0].scaleX = 1
+        this.Platform[0].x = -115;
+        this.Platform[1].scaleX = Math.random() * (this.max_scale_plat - this.min_scale_plat) + this.min_scale_plat
+        this.Platform[1].x = Math.random() * (this.max_plat - this.min_plat) + this.min_plat + this.Platform[0].width / 2 + this.Platform[1].scaleX * this.Platform[1].width / 2
+        this.game_over.node.active = false
+        this.gameStatus = GameStatus.Game_Playing
+        this.Best_score_img.node.active = false
+        this.Best_score.node.active = false
+        this.replay.node.active = false
+        this.Platform_parent.active = true
+        this.score_num = 0
+        this.score.string = this.score_num.toString()
+        this.monkey.node.x = -120
+        this.monkey.node.y = 0
+        this.monkey.node.angle = 0
+        this.plat_number = 0
+        this.stick.node.x = this.Platform[this.plat_number].scaleX * this.Platform[this.plat_number].width / 2 - 120
+        this.is_ok = false
+        this.monkey_die = false
     }
 }
